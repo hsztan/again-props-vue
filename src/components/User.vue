7c5b315a460dc2cefa13037c4c9cad8ac2534b18
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-09-09 08:54:31 -0500

    props validation and custom validations

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,15 +1,28 @@
 <template>
   <button type="button" @click="onClickAge">Update age</button>
   <p>The user is {{ age }} years old.</p>
+  <p>Age doubled: {{ ageDoubled }}</p>
 </template>
 
 <script>
 export default {
   name: 'User',
   props: {
-    age: Number
+    age: {
+      type: Number,
+      // required: true,
+      // default: 20,
+      validator(value) {
+        return value < 130
+      }
+    }
   },
   emits: ['age-change'],
+  computed: {
+    ageDoubled() {
+      return this.age * 2
+    }
+  },
   methods: {
     onClickAge() {
       this.$emit('age-change', 3)
