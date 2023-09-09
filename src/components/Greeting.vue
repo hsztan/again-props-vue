5b52e7deef2f1dd4134f312b169e1fce9867e09a
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-09-08 19:19:00 -0500

    first commit

---
+++ src/components/Greeting.vue
@@ -0,0 +1,24 @@
+<template>
+  <p v-if="age >= 25">{{ msg }}</p>
+  <p v-else>You must be 25 years or older to view this message.</p>
+</template>
+
+<script>
+export default {
+  name: 'Greeting',
+  props: {
+    age: Number
+  },
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+p:hover {
+  color: darken($color: #cc4444, $amount: 15%)
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <button type="button" @click="onClickAge">Update age</button>
+  <p>The user is {{ age }} years old.</p>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  props: {
+    age: Number
+  },
+  emits: ['age-change'],
+  methods: {
+    onClickAge() {
+      this.$emit('age-change', 3)
+    }
+  }
+}
+</script>
